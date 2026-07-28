d1fff521bcf8fcb07f041004c21a7d926366dd0d
2yt <2yt.dev.con@gmail.com>
2026-07-28 20:52:40 +0330

    Setuping `DEFAULT_AUTHENTICATION_CLASSES` for `authtoken`

--- config/envs/development.py
+++ config/envs/development.py
@@ -30,4 +30,7 @@ SPECTACULAR_SETTINGS = {
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
 }
