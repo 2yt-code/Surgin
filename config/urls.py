3b53058edf6649b57662412d3e7c4d93243f61a2
2yt <2yt.dev.con@gmail.com>
2026-07-28 20:54:34 +0330

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -8,20 +8,15 @@ i18n_urls = [
     path('i18n/', include('django.conf.urls.i18n'))
 ]
 
-v1_admin_urls = []
-v1_frontend_urls = [
-    path('v1/artist/', include('apps.v1.artist.urls')),
-    path('v1/account/', include('apps.v1.account.urls')),
-]
-
 doc_patterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
-urlpatterns = [] # + v1_admin_urls + v1_frontend_urls + doc_patterns
+urlpatterns = [] + i18n_urls + doc_patterns
 urlpatterns += i18n_patterns(
-    path('v1/artist/', include('apps.v1.artist.urls')),
+    # v1
     path('v1/account/', include('apps.v1.account.urls')),
+    path('v1/artist/', include('apps.v1.artist.urls')),
 )
