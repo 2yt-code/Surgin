ff9450d49013489636e055eadfbf00e965330c7e
2yt <2yt.dev.con@gmail.com>
2026-07-28 20:51:02 +0330

    Adding `Login` view

--- apps/v1/account/views/frontend.py
+++ apps/v1/account/views/frontend.py
@@ -1,13 +1,40 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login
 from rest_framework import status
 from rest_framework import generics
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
-from apps.v1.account.serializers import RegisterSerializer
+from apps.v1.account.serializers import (
+    RegisterSerializer,
+    LoginSerializer
+)
 
 
 User = get_user_model()
 
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.validated_data.get('user')
+            token, created = Token.objects.get_or_create(user=user)
+            login(request, user)
+
+            return Response(
+                {'token': token.key},
+                status=status.HTTP_200_OK
+                )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+            
+    
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
