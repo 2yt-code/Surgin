14107357f340c667f91ae7ab50f03601a6b433b3
Kasra Jokar <2yt.dev.con@gmail.com>
2026-08-20 15:25:39 +0330

    Update views

--- apps/v1/account/views/frontend.py
+++ apps/v1/account/views/frontend.py
@@ -10,12 +10,13 @@ from rest_framework import (
 )
 from apps.v1.account.serializers import (
     RegisterSerializer,
-    ProfileSerializer
+    ProfileSerializer,
 )
 from apps.v1.account.models import FingerPrint
+import utils
 
 
-User = get_user_model()    
+User = get_user_model()
 
 class CustomTokenRefreshView(TokenRefreshView):
     throttle_classes = UserRateThrottle
@@ -34,7 +35,8 @@ class RegisterView(generics.CreateAPIView):
         fingerprint = FingerPrint.objects.create(
             user_id=user.id,
             ip_address=request.META.get('REMOTE_ADDR'),
-            user_agent=request.META.get('HTTP_USER_AGENT', ''),
+            user_agent=utils.fingerprint.create(request.META.get('HTTP_USER_AGENT', '')),
+            trust_level=100
         )
         fingerprint.save()
 
