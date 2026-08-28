eb391c3a9702bda1961ba716465723210ecf52ed
Kasra Jokar <2yt.dev.con@gmail.com>
2026-08-28 03:38:25 +0330

    Update: `Fingerprint` fields

--- apps/v1/account/models.py
+++ apps/v1/account/models.py
@@ -1,27 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
+from device_tracker.models import Device
 
 
 class Membership(AbstractUser):
     first_name = models.CharField(
         _("first name"), 
         max_length=150,
-        help_text=_('Required first name. 150 characters')
+        help_text=_("Required first name. 150 characters")
     )
     last_name = models.CharField(
         _("last name"), 
         max_length=150, 
-        help_text=_('Required last name. 150 characters')
+        help_text=_("Required last name. 150 characters")
     )
     email = models.EmailField(
         _("email address"), 
-        help_text=_('Required email. 254 characters')
+        help_text=_("Required email. 254 characters")
     )
     password = models.CharField(
         _("password"), 
         max_length=128,
-        help_text=_('Required password. 128 characters')
+        help_text=_("Required password. 128 characters")
     )
     premium = models.BooleanField(default=False)
 
@@ -29,12 +30,28 @@ class Membership(AbstractUser):
         return self.username
 
 class FingerPrint(models.Model):
-    ip_address = models.GenericIPAddressField(_("ip address"))
-    user_agent = models.TextField(_("user agent"))
-    created_at = models.DateTimeField(_("created"), auto_now_add=True)
-    trust_level = models.IntegerField(_("trust level"), default=0)
-    user_id = models.IntegerField()
-
+    device = models.OneToOneField(
+        Device,
+        verbose_name=_("device"),
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    key = models.CharField(
+        _("fingerprint hash"),
+        max_length=250
+    )
+    created_at = models.DateTimeField(
+        _("created"), 
+        auto_now_add=True
+    )
+    trust_score = models.IntegerField(
+        _("trust level"), 
+        default=50
+    )
+    last_verified_at = models.DateTimeField(
+        auto_now_add=True
+    )
     class Meta:
         verbose_name = _("fingerprint")
         verbose_name_plural = _("fingerprints")
