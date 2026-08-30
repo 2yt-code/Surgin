164c301f979871aabc2860ca057087b9467d26fc
Kasra Jokar <2yt.dev.con@gmail.com>
2026-08-31 03:23:52 +0330

    Update: Customizing the default `DeviceLogoutView` class from the `django-device-tracker` library for account logout operations

--- apps/v1/account/views/frontend.py
+++ apps/v1/account/views/frontend.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from django.utils.translation import gettext_lazy as _
+from device_tracker.views import DeviceLogoutView
+from device_tracker.models import Device
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
 from rest_framework.throttling import UserRateThrottle
 from rest_framework.response import Response
 from rest_framework import (
@@ -8,16 +12,42 @@ from rest_framework import (
     generics,
     permissions,
 )
+from apps.v1.account.models import FingerPrint
 from apps.v1.account.serializers import (
     RegisterSerializer,
     ProfileSerializer,
 )
+import utils
 
 
 User = get_user_model()
 
 class CustomTokenRefreshView(TokenRefreshView):
     throttle_classes = (UserRateThrottle,)
+
+class CustomDeviceLogoutView(DeviceLogoutView):
+    def get(self, request):
+        key = utils.fingerprint.scheme_key(request)
+
+        get_fingerprint_model = FingerPrint.objects.filter(key=key).first()
+        if get_fingerprint_model:
+            device = get_object_or_404(Device, pk=get_fingerprint_model.pk, user=request.user)
+        else: pass # TODO Resolving the "not found in database" issue
+
+        if device.refresh_token_jti:
+            try:
+                token = OutstandingToken.objects.get(jti=device.refresh_token_jti)
+                BlacklistedToken.objects.get_or_create(token=token)
+            except OutstandingToken.DoesNotExist:
+                pass
+
+        get_fingerprint_model.delete()
+        device.delete()
+
+        return Response(
+            {'detail': 'device logged out'},
+            status=status.HTTP_200_OK
+        )
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -33,9 +63,7 @@ class RegisterView(generics.CreateAPIView):
             status=status.HTTP_201_CREATED
             )
  
-class ProfileView(generics.RetrieveUpdateAPIView):
+class ProfileView(generics.ListAPIView):
     serializer_class = ProfileSerializer
+    queryset = User.objects.all()
     permission_classes = (permissions.IsAuthenticated,)
-
-    def get_object(self):
-        return self.request.user.id
