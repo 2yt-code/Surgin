9e14a46f4a019f5ebb9f4cc7e6215a329ea73aa5
Kasra Jokar <2yt.dev.con@gmail.com>
2026-08-31 03:26:40 +0330

    Adding: `scheme_key` method for fingerprint key

--- utils/fingerprint.py
+++ utils/fingerprint.py
@@ -2,6 +2,23 @@ import hmac
 import hashlib
 from django.conf import settings
 
+import utils
+
+
+def scheme_key(request):
+    device_info = utils.device_info.get(
+        request,
+        request.META.get('HTTP_USER_AGENT')
+    )
+
+    user_agent = device_info.get('user_agent')
+    browser = device_info.get('browser')
+    platform = device_info.get('platform')
+    device_type = device_info.get('device_type')
+    key = create(f'{user_agent}:{browser}:{platform}:{device_type}')
+
+    return key
+    
 def create(user_agent: str):
     return hmac.new(
         settings.SECRET_KEY.encode(),
