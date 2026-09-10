1483aff63df4f7d6994d217c289c92a9f61c6e01
Kasra Jokar <2yt.dev.con@gmail.com>
2026-09-10 14:55:04 +0330

    Update urls

--- apps/api/v1/artist/urls/frontend.py
+++ apps/api/v1/artist/urls/frontend.py
@@ -3,5 +3,5 @@ from apps.api.v1.artist import views
 
 
 urlpatterns = [
-    path('register/', views.RegisterArtistAccountView.as_view(), name='register-artist')
+    # path('register/', views.RegisterArtistAccountView.as_view(), name='register-artist')
 ]
