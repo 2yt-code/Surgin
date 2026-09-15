6f1aeb30b393744234f786823784d8a74bc6786a
Kasra Jokar <2yt.dev.con@gmail.com>
2026-09-15 03:58:38 +0330

    Deleting for new paths format

--- apps/api/v1/artist/urls/frontend.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from apps.api.v1.artist import views
-
-
-urlpatterns = [
-    # path('register/', views.RegisterArtistAccountView.as_view(), name='register-artist')
-]
