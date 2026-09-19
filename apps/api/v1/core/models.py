c7941f7bd07eb850d3c64fcb9780f5514001abd1
Kasra Jokar <2yt.dev.con@gmail.com>
2026-09-20 03:08:56 +0330

    Adding `Song` table

--- apps/api/v1/core/models.py
+++ apps/api/v1/core/models.py
@@ -1,22 +1,24 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-from apps.api.v1.core.artist.models import Artist
+# from apps.api.v1.core.artist.models import Artist
+# from apps.api.v1.core.album.models import Album
 
-
+# TODO
 class Song(models.Model):
     title = models.CharField(max_length=200)
-    artist = models.ForeignKey(
-        Artist,
-        on_delete=models.CASCADE,
-        related_name="songs"
-    )
-    album = models.ForeignKey(
-        #Album,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name="songs"
-    )
+    # artist = models.ForeignKey(
+    #     Artist,
+    #     on_delete=models.CASCADE,
+    #     related_name="songs"
+    # )
+    # album = models.ForeignKey(
+    #     Album,
+    #     on_delete=models.SET_NULL,
+    #     null=True,
+    #     blank=True,
+    #     related_name="songs"
+    # )
     cover = models.ImageField(
         upload_to="song_covers/",
         null=True,
@@ -35,4 +37,9 @@ class Song(models.Model):
     like_count = models.PositiveBigIntegerField(default=0)
     is_published = models.BooleanField(default=True)
     duration = models.DurationField()
-    updated_at = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("song")
+        verbose_name_plural = _("songs")
+        db_table = 'song'
